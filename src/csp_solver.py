eaf7634bd8d832e829fcd21ceed1c2028ec69207
herashiniraman <herashini.raman@qiu.edu.my>
2026-08-16 19:27:24 +0800

    Add MRV heuristic to improve CSP scheduling efficiency

--- src/csp_solver.py
+++ src/csp_solver.py
@@ -1,9 +1,7 @@
-from models import Volunteer, FoodPickup
-
-
 class CSPSolver:
 
     def __init__(self, volunteers, pickups):
+
         self.volunteers = volunteers
         self.pickups = pickups
         self.solution = {}
@@ -11,17 +9,17 @@ class CSPSolver:
 
     def check_constraints(self, volunteer, pickup):
 
-        # Check vehicle capacity
+        # Vehicle capacity constraint
         if volunteer.vehicle_capacity < pickup.food_weight:
             return False
 
 
-        # Check availability
+        # Availability constraint
         if volunteer.available_time != pickup.pickup_time:
             return False
 
 
-        # Check assignment conflict
+        # One volunteer cannot handle multiple pickups
         if volunteer.name in self.solution.values():
             return False
 
@@ -29,38 +27,90 @@ class CSPSolver:
         return True
 
 
+
+    # MRV Heuristic
+    # Select pickup with the fewest possible volunteers first
+
+    def select_mrv_order(self):
+
+        pickup_options = []
+
+
+        for pickup in self.pickups:
+
+            possible_volunteers = []
+
+
+            for volunteer in self.volunteers:
+
+                if self.check_constraints(volunteer, pickup):
+
+                    possible_volunteers.append(volunteer)
+
+
+            pickup_options.append(
+                (pickup, len(possible_volunteers))
+            )
+
+
+        pickup_options.sort(
+            key=lambda x: x[1]
+        )
+
+
+        return [
+            item[0]
+            for item in pickup_options
+        ]
+
+
+
     def solve(self):
+
+        self.pickups = self.select_mrv_order()
 
         return self.backtrack(0)
 
 
-    def backtrack(self, pickup_index):
 
-        # Goal test:
-        # All pickups assigned
-        if pickup_index == len(self.pickups):
+    def backtrack(self, index):
+
+        # Goal: all pickups assigned
+
+        if index == len(self.pickups):
+
             return True
 
 
-        pickup = self.pickups[pickup_index]
+        pickup = self.pickups[index]
 
 
         for volunteer in self.volunteers:
 
+
             if self.check_constraints(volunteer, pickup):
 
-                # Assign volunteer
+
+                # Assign
+
                 self.solution[pickup.restaurant] = volunteer.name
 
 
+
                 # Continue searching
-                if self.backtrack(pickup_index + 1):
+
+                if self.backtrack(index + 1):
+
                     return True
 
 
-                # Backtrack
+
+                # Undo assignment (backtracking)
+
                 del self.solution[pickup.restaurant]
 
 
+
         return False
+
     
